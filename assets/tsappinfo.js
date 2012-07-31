7f4461a880da694016aae17e892d73cbbe959ccc
Chris Dent <chris.dent@gmail.com>
2012-07-31 14:57:23 +0100

    Present config and command info via tiddlers
    For the purposes of demonstrating getting content via
    a tsapp and continuing testing.

--- assets/tsappinfo.js
+++ assets/tsappinfo.js
@@ -4,5 +4,16 @@ $(function() {
     $('dl.tabs dt').click(function() {
         $(this).siblings().removeClass('selected').end()
         .next('dd').andSelf().addClass('selected');
+
+        currentDL = $(this);
+
+        var tiddler = $(this).text();
+        $.ajax({
+            url: '/bags/tsapp_public/tiddlers/' + tiddler + '.json?render=1',
+            success: function(data) {
+                var tiddlerInfo = $('<div>').append(data.render);
+                currentDL.next('dd').empty().append(tiddlerInfo);
+            }
+        });
     });
 });
